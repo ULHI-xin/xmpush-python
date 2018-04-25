d317cac8666677a1f3c7edcd015d7c1826c0c8f9
ULHI-Xin <xin@upwlabs.com>
2018-04-25 11:42:57 +0800

    update cfg for dist via pip

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
-from xmpush.base.APIConstants import Constants
 
 kw = dict(
     name='xmpush-python',
-    version=Constants.__VERSION__,
-    description='xiaomi push python sdk',
-    author='mi_push',
-    author_email='mipush-support@xiaomi.com',
-    url='http://dev.xiaomi.com/mipush/downpage',
-    download_url='http://dev.xiaomi.com/mipush/downpage',
+    version='1.0.3',
+    description='Python3 xiaomi push sdk',
+    author='hatevol',
+    author_email='hatevol@gmail.com',
+    url='https://github.com/ULHI-xin/xmpush-python',
     packages=['xmpush', 'xmpush.base'],
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
